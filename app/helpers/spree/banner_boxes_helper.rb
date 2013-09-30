10b005aaad8047f9bd86b3f6a861238b0fcf1e19
nebirhos <me@nebirhos.com>
2013-09-30 14:39:27 +0200

    Spree::BannerBox.enabled accepts multiple categories

--- app/helpers/spree/banner_boxes_helper.rb
+++ app/helpers/spree/banner_boxes_helper.rb
@@ -6,19 +6,19 @@ module Spree
       params[:class] ||= "banner"
       params[:style] ||= "small"
       params[:list] ||= false
-      @@banner = Spree::BannerBox.enable(params[:category])
+      @@banner = Spree::BannerBox.enabled(params[:category])
       if @@banner.blank?
         return ''
       end
       res = []
       banner = @@banner.sort_by { |ban| ban.position }
-        
+
       if (params[:list])
         content_tag(:ul, banner.map{|ban| content_tag(:li, link_to(image_tag(ban.attachment.url(params[:style].to_sym)), (ban.url.blank? ? "javascript: void(0)" : ban.url)), :class => params[:class])}.join().html_safe )
       else
         banner.map{|ban| content_tag(:div, link_to(image_tag(ban.attachment.url(params[:style].to_sym)), (ban.url.blank? ? "javascript: void(0)" : ban.url)), :class => params[:class])}.join().html_safe
       end
     end
-    
+
   end
 end
